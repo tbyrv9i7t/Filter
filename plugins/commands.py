9e23bb1730c6e260f0c874f89657be663b45aedb
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-06 19:11:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -229,10 +229,6 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-        buttons = [[
-            InlineKeyboardButton('◀️ Back', callback_data='start')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
