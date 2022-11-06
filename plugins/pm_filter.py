22c1c4de58f85dd08a2974040b0049e8cb8cda2e
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-06 17:53:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -342,7 +342,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.answer(alert, show_alert=True)
     if query.data.startswith("file"):
         ident, file_id = query.data.split("#")
-        user = query.message.reply_to_message.from_user.id
+        user = query.message.reply_to_message.id
         if int(user) != 0 and query.from_user.id != int(user):
             return await query.answer("This Was Not Your Request, Request Yourself", show_alert=True)
         if not files_:
