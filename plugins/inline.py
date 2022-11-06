4c2475c94dcb9a9896bdc04c5ed184ad45b775cc
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-06 08:41:48 +0530

    Filter Bot

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,14 +26,14 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='okDa',
+                           switch_pm_text='Only Admins can use Bot Inline Search!',
                            switch_pm_parameter="hehe")
         return
 
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You have to Subscribe my Channel to use the Bot!',
                            switch_pm_parameter="subscribe")
         return
 
