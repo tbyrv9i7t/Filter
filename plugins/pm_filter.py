12a361b4a77c99f7bdd9727298bfc76751e4e9f1
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-06 19:09:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -354,14 +354,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         f_caption = files.caption
         settings = await get_settings(query.message.chat.id)
         if CUSTOM_FILE_CAPTION:
-        buttons = [[
-            InlineKeyboardButton('◀️ Back', callback_data='start')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
             try:
                 f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
                                                        file_size='' if size is None else size,
                                                        file_caption='' if f_caption is None else f_caption)
+        buttons = [[
+            InlineKeyboardButton('◀️ Back', callback_data='start')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
             except Exception as e:
                 logger.exception(e)
             f_caption = f_caption
