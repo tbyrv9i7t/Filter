d89569b25b37e6b23a527ab7f0adabd3e43189a9
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-06 13:11:50 +0530

    Filter Bot

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -360,6 +360,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f_caption
         if f_caption is None:
             f_caption = f"{files.file_name}"
+        buttons = [[
+            InlineKeyboardButton('Please Share and Support', url='https://t.me/share/url?url=Movie%20Bot:%20@SL_Auto_Filter_Bot')
+        ]]
 
         try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
