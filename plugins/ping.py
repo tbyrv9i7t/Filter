47cb8bec763413629c6bbf58cc8cb7099a8aec84
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 11:25:39 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -7,22 +7,10 @@ HELP = 'HIIII'
 
 @Client.on_message(filters.command('help'))
 async def alive(bot, message):
-    buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Group ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search Movie', switch_inline_query_current_chat='')
-            ],[
-            InlineKeyboardButton('❓ Help', callback_data='help'),
-            InlineKeyboardButton('ℹ️ About', callback_data='about')
-            ],[
-            InlineKeyboardButton('❌ Close ❌', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
+    btn = [[InlineKeyboardButton('HIII', url='https://github.com/Hansaka-Anuhas/Filter-Bot/blob/master/plugins/pm_filter.py')
+           ]]
+    )
+    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+                    reply_markup=InlineKeyboardMarkup(btn))
 
 
