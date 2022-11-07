db38e956586c88360039b737b2e7d1a2b3dcec36
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 13:13:05 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -208,3 +208,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
     else:
         await quer_y.message.edit(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
     await quer_y.answer()
+
+@Client.on_message(filters.command('about_creator'))
+async def about_creator(bot, message):
+    await message.reply(script.ABOUTCREATOR_TXT)
