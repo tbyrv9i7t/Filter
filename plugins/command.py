0a0315ab79c399dfe6cd83261fb0b61b4e6ae288
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 12:26:55 +0530

    Update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
 
+
+ABOUT_CREATOR = 'Full Name: Hansaka Anuhas\nAge: 16\nLive in: Sri Lanka 🇱🇰'
+
+@Client.on_message(filters.command('about_creator'))
+async def about_creator(bot, message):
+    await message.reply_text(ABOUT_CREATOR)
