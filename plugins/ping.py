c94dd035db5698f6c4dd62532f682d1857b8ba54
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 11:32:52 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,15 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
+
 HELP = 'HIIII'
-
-
 
 @Client.on_message(filters.command('help'))
 async def alive(bot, message):
-    btn = [[InlineKeyboardButton('HIII', url='https://github.com/Hansaka-Anuhas/Filter-Bot/blob/master/plugins/pm_filter.py')
+    buttons = [[
+        InlineKeyboardButton('HIII', url='https://github.com/Hansaka-Anuhas/Filter-Bot/blob/master/plugins/pm_filter.py')
            ]]
-    await message.reply("I couldn't find anything related to that\nDid you mean any one of these?",
-                    reply_markup=InlineKeyboardMarkup(btn))
-
-
+    await message.reply(HELP, reply_markup = InlineKeyboardMarkup(buttons))
