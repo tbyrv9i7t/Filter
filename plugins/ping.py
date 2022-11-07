6d0765c2bc810310c81b57e59603e19928263d13
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 10:20:48 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,11 +1,1 @@
-import time
-import random
-from pyrogram import Client, filters
 
-@Client.on_message(filters.command('ping'))
-async def ping(_, message):
-    start_t = time.time()
-    rm = await message.reply_text("...")
-    end_t = time.time()
-    time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Your Ping - {time_taken_s:.3f} ms")
