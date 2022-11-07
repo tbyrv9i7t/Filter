2788fedfe695cecc8a286e9c5ad5a40768fba875
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 10:54:49 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.command("alive", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("alive"))
 async def check_alive(_, message):
     await message.reply_text('hii')
