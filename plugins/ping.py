4969b7fefba1e6bc44bf9ffeb9725203d48f4dcd
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 11:28:12 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -9,7 +9,7 @@ HELP = 'HIIII'
 async def alive(bot, message):
     btn = [[InlineKeyboardButton('HIII', url='https://github.com/Hansaka-Anuhas/Filter-Bot/blob/master/plugins/pm_filter.py')
            ]]
-    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+    await message.reply("I couldn't find anything related to that\nDid you mean any one of these?",
                     reply_markup=InlineKeyboardMarkup(btn))
 
 
