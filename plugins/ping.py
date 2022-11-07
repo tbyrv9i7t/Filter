9e650c8ea313d60fbe7219e4cda484218f087e7d
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 11:52:56 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -2,11 +2,11 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 
-HELP = 'HIIII'
+HELP_TXT = 'HIIII'
 
 @Client.on_message(filters.command('help'))
 async def alive(bot, message):
     btn = [[
         InlineKeyboardButton('Contact Me', url='https://t.me/Hansaka_Anuhas')
            ]]
-    await message.reply_photo(photo='https://telegra.ph/file/6cdc6dee747e610adc994.jpg', reply_markup=InlineKeyboardMarkup(btn))
+    await message.reply_sticker(sticker='CAACAgIAAxkBAAEGUXdjZlg70pKyL3ciahu5T8GL-5tbeAACBQADwDZPE_lqX5qCa011KwQ', text='hii', reply_markup=InlineKeyboardMarkup(btn))
