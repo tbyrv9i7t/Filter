d0ed9175a886c368e9472a9ce6d34d6727f3d101
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 13:13:16 +0530

    Delete extra_commands.py

--- plugins/extra_commands.py
+++
@@ -1,6 +0,0 @@
-from Script import script
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command('about_creator'))
-async def about_creator(bot, message):
-    await message.reply(script.ABOUT_CREATOR)
