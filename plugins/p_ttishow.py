5e29219f51b57b43f4de576bc5c5f013ac43ac40
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 20:16:21 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -131,7 +131,7 @@ async def re_enable_chat(bot, message):
 
 @Client.on_message(filters.command('status') & filters.incoming)
 async def get_status(bot, message):
-    rju = await message.reply('Fetching stats..')
+    rju = await message.reply('Getting status...')
     total_users = await db.total_users_count()
     totl_chats = await db.total_chat_count()
     files = await Media.count_documents()
