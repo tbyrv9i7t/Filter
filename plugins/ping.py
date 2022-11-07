811514337d45c340baca6f98c30f624878c52462
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-07 11:44:57 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -9,4 +9,4 @@ async def alive(bot, message):
     btn = [[
         InlineKeyboardButton('Contact Me', url='https://t.me/Hansaka_Anuhas')
            ]]
-    await message.reply.photo(photo'https://telegra.ph/file/6cdc6dee747e610adc994.jpg', reply_markup=InlineKeyboardMarkup(btn))
+    await message.reply.photo(photo='https://telegra.ph/file/6cdc6dee747e610adc994.jpg', reply_markup=InlineKeyboardMarkup(btn))
