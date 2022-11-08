8125bea56c6ca479aa60acaa8d4f46675c94a970
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-08 12:34:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -40,7 +40,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("හෙලෝ {query.from_user.full_name}, අනුන්ගේ ඒවා ඔබන්න එපා!", show_alert=True)
+        return await query.answer("හෙලෝ {query.from_user.mention}, අනුන්ගේ ඒවා ඔබන්න එපා!", show_alert=True)
     try:
         offset = int(offset)
     except:
