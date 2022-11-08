1d3d1aa4d56a856f06df0f9206a79edc583289a7
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-08 10:07:08 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -231,7 +231,7 @@ async def list_users(bot, message):
     users = await db.get_all_users()
     out = "Users Saved In DB Are:\n\n"
     async for user in users:
-        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>`{user['id']}`\n"
+        out += f"**Name:** {user['name']}\n**ID:** `{user['id']}`\n"
         if user['ban_status']['is_banned']:
             out += '( Banned User )'
         out += '\n'
