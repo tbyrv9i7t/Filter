32223330fb24f088309a88cebbdf7b3e2d30d4e9
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-08 11:51:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -40,7 +40,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("This is not for you! Request yourself.", show_alert=True)
+        return await query.answer("හෙලෝ message.from_user.mention, අනුන්ගේ ඒවා ඔබන්න එපා!", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -131,7 +131,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("This is not for you! Request yourself.", show_alert=True)
+        return await query.answer("හෙලෝ message.from_user.mention, අනුන්ගේ ඒවා ඔබන්න එපා!", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.id)
