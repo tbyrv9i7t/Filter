a4de3c7c61bb50decfe0fa4cb05f2d439ad006e5
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-11 12:01:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -233,10 +233,10 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        protect_content=True if pre == 'filep' else False
-    )
-    await message.reply(text='hii'),
-   
+        protect_content=True if pre == 'filep' else False,
+        await message.reply(text='hii'),
+        )
+    
 
 
 @Client.on_message(filters.command('channels') & filters.user(ADMINS))
