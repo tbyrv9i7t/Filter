9a648686f5a7ee85df0775f7a5712bcb3817ba1e
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-11 20:28:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,7 +37,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention),
+            caption=f"<b>👋 හෙලෝ {message.from_user.mention}</b>,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
