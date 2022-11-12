669d3f3f59a238e49f4d1f67af1440d212dd749a
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-12 21:45:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -33,6 +33,7 @@ SPELL_CHECK = {}
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
+        settings = await get_settings(message.chat.id)
         await auto_filter(client, message) if settings['auto_ffilter'] else None
 
 
