04479d6a4087f6799e511716669aaa5e9b8a9f76
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-13 08:47:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -752,18 +752,23 @@ async def auto_filter(client, msg, spoll=False):
     if imdb and imdb.get('poster'):
         try:
             a = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-
+            await asyncio.sleep(10)
+            await b.delete() if settings['auto_delete'] else None
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            b = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            await asyncio.sleep(10)
+            await b.delete() if settings['auto_delete'] else None
         except Exception as e:
             logger.exception(e)
-            await message.reply_photo(photo="https://telegra.ph/file/cbcaa5500a0d3cee10d07.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            c = await message.reply_photo(photo="https://telegra.ph/file/cbcaa5500a0d3cee10d07.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            await asyncio.sleep(10)
+            await c.delete() if settings['auto_delete'] else None
     else:
-        b = await message.reply_photo(photo="https://telegra.ph/file/cbcaa5500a0d3cee10d07.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+        d = await message.reply_photo(photo="https://telegra.ph/file/cbcaa5500a0d3cee10d07.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(10)
-        await b.delete() if settings['auto_delete'] else None
+        await d.delete() if settings['auto_delete'] else None
     if spoll:
         await msg.message.delete()
 
