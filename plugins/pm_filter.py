57a16cd2d2d6a64ad0a09018e5a66b54228c0062
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-13 21:12:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -767,11 +767,11 @@ async def auto_filter(client, msg, spoll=False):
             await asyncio.sleep(3600)
             await c.delete() if settings['auto_delete'] else None
     else:
-        d = await message.reply_photo(photo="https://telegra.ph/file/f1da7639c429a6a0dba80.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+        d = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(3600)
         await d.delete() if settings['auto_delete'] else None
-        if spoll:
-            await msg.message.delete()
+    if spoll:
+        await msg.message.delete()
 
 
 async def advantage_spell_chok(msg):
