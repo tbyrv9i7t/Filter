5480970950c5c5686cfcfb3c2197132cec95a4f8
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-14 09:43:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -773,7 +773,7 @@ async def auto_filter(client, msg, spoll=False):
                     await asyncio.sleep(600)
                     await fuk.delete()
                     await message.delete()
-            except KeyError:
+        except KeyError:
                 grpid = await active_connection(str(message.from_user.id))
                 await save_group_settings(grpid, 'auto_delete', True)
                 settings = await get_settings(message.chat.id)
