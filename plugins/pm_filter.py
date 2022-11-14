46d1bc299a4aa22f7618682eb2336d9c60f79a68
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-14 10:11:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -767,13 +767,11 @@ async def auto_filter(client, msg, spoll=False):
             await asyncio.sleep(3600)
             await k.delete() if settings['auto_delete'] else None
     else:
-        fuk = await message.reply_photo(photo="https://telegra.ph/file/f1da7639c429a6a0dba80.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-        try:
-            if settings['auto_delete']:
-                await asyncio.sleep(600)
-                await fuk.delete()
-                await message.delete()
-        except
+        k = await message.reply_photo(photo="https://telegra.ph/file/f1da7639c429a6a0dba80.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(3600)
+        await k.delete() if settings['auto_delete'] else None
+
+
     if spoll:
        await msg.message.delete()
 
