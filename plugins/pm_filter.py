facce9800a896a277014bdc5a3826cc390fa1bb9
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-14 09:37:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -768,8 +768,9 @@ async def auto_filter(client, msg, spoll=False):
             await k.delete() if settings['auto_delete'] else None
     else:
         k = await message.reply_photo(photo="https://telegra.ph/file/f1da7639c429a6a0dba80.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-  else
-      if spoll:
+    else:
+        await message.reply_text('hi')
+    if spoll:
         await msg.message.delete()
 
 
