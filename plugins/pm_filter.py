69e645906d974da4a5c3ea4fcac215e659e01c67
Hansaka Anuhas <117273467+Hansaka-Anuhas@users.noreply.github.com>
2022-11-14 10:19:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -771,8 +771,8 @@ async def auto_filter(client, msg, spoll=False):
         await asyncio.sleep(3600)
         await k.delete() if settings['auto_delete'] else None
        except KeyError:
-        await asyncio.sleep(3600)
-        await k.delete() if settings['auto_delete'] else None
+        k = await message.reply_photo(photo="https://telegra.ph/file/f1da7639c429a6a0dba80.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+
 
 
     if spoll:
